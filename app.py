35d40f23fa2040881c1752a93ab4fd4c9dbd1e7a
ihsanhabibullah <ihsan.habibullah40@smk.belajar.id>
2025-09-27 09:11:53 +0700

    membuat base html

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+data={
+    "nama":"kumar",
+    "tempat_lahir":"Terban",
+    "tanggal_lahir":"17-8-1945",
+    "jenis_kelamin":"laki-laki",
+    "alamat":"Depok",
+    "profesi":"Jungler mpl"
+}
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', title="Halaman index")
-   
+    return render_template('index.html', title="Home",isi="Selamat Datang Di Web Kagaku No Hikari")
+
+@app.route('/biodata')
+def biodata() :
+    return render_template('biodata.html',title="Biodata",isi=data)
+
 @app.route('/about')
 def about(): 
-    return "<center><h1>ini adalah halaman about<h1><center>"
-
-@app.route('/halo/<name>')
-def hali(name) :
-    return f"<center><h1>halo {name} !! pue haba<h1><center> "
+    return render_template('about.html',title="About",isi="Ini adalah halaman About")
 
 if __name__ == '__main__' :
     app.run(debug=True)
